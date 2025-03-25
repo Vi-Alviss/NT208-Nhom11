02d6db4a36c7bcea5b0bb70513f50aad7a3c294e
LQH <lqhu64@gmail.com>
2025-03-26 01:28:47 +0700

    bo sung quen mat khau

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,39 +1,61 @@
-const firebaseAdmin = require('firebase-admin');
-const { db } = require('../config/firebase');
+const { getAuth, signInWithEmailAndPassword } = require('firebase/auth');
+const { initializeApp } = require('firebase/app');
+
+const { admin } = require('../config/firebase');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-gWnMuwF8s-7ZAyvO_wU4uJUp1JTMM_s",
+    authDomain: "login-backend-3f181.firebaseapp.com",
+    projectId: "login-backend-3f181",
+    storageBucket: "login-backend-3f181.firebasestorage.app",
+    messagingSenderId: "109807957436",
+    appId: "1:109807957436:web:2336f6db236d97c8bd67b9",
+    measurementId: "G-D6B7EPE87X"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
 
 const login = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ message: "Thiếu email hoặc mật khẩu!" });
+        return res.status(400).json({ message: "Chưa nhập email hoặc mật khẩu!" });
     }
 
     try {
-        const user = await firebaseAdmin.auth().getUserByEmail(email);
-        const uid = user.uid;
-        const userRef = db.collection('users').doc(uid);
-        const doc = await userRef.get();
-
-        if (!doc.exists) {
-            await userRef.set({
-                email: user.email,
-                name: user.displayName || "Người dùng chưa đặt tên",
-                createdAt: new Date()
-            });
-        }
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
 
         return res.json({
             message: "Đăng nhập thành công!",
             user: {
-                uid: uid,
+                uid: user.uid,
                 email: user.email,
                 name: user.displayName || "Người dùng chưa đặt tên"
             }
         });
 
     } catch (error) {
-        return res.status(401).json({ message: "Email không tồn tại hoặc có lỗi!", error: error.message });
+        return res.status(401).json({ message: "Email hoặc mật khẩu không đúng!", error: error.message });
     }
 };
 
-module.exports = { login };
+
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: "Vui lòng nhập email!" });
+    }
+
+    try {
+        const resetLink = await admin.auth().generatePasswordResetLink(email);
+        return res.json({ message: "Đã gửi tới email!", resetLink });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Lỗi gửi email!", error: error.message });
+    }
+};
+
+module.exports = { login, forgotPassword };
